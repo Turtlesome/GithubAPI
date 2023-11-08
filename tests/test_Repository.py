ada530375d1e87fba76519bfc3970a2c44cba6ba
Mateusz Kasprzykowski <kasprzykowskimateusz13@gmail.com>
2023-11-08 21:21:53 +0100

    Added fixture to test_Repository

--- tests/test_Repository.py
+++ tests/test_Repository.py
@@ -2,35 +2,35 @@ import pytest
 from classes.Repository import Repository
 
 
-def test_getRepositoryName():
-    repo = Repository('TestRepo', 100, '2023-01-01', '2023-01-02')
-    assert repo.getRepositoryName() == 'TestRepo'
+@pytest.fixture
+def test_repository():
+    return Repository('TestRepo', 100, '2023-01-01', '2023-01-02')
 
 
-def test_getStarsCount():
-    repo = Repository('TestRepo', 100, '2023-01-01', '2023-01-02')
-    assert repo.getStarsCount() == 100
+def test_getRepositoryName(test_repository):
+    assert test_repository.getRepositoryName() == 'TestRepo'
 
 
-def test_getCreationDate():
-    repo = Repository('TestRepo', 100, '2023-01-01', '2023-01-02')
-    assert repo.getCreationDate() == '2023-01-01'
+def test_getStarsCount(test_repository):
+    assert test_repository.getStarsCount() == 100
 
 
-def test_getLastUpdatedDate():
-    repo = Repository('TestRepo', 100, '2023-01-01', '2023-01-02')
-    assert repo.getLastUpdatedDate() == '2023-01-02'
+def test_getCreationDate(test_repository):
+    assert test_repository.getCreationDate() == '2023-01-01'
 
 
-def test_to_dict():
-    repo = Repository('TestRepo', 100, '2023-01-01', '2023-01-02')
+def test_getLastUpdatedDate(test_repository):
+    assert test_repository.getLastUpdatedDate() == '2023-01-02'
+
+
+def test_to_dict(test_repository):
     expected_dict = {
         "name": "TestRepo",
         "stars": 100,
         "created_at": "2023-01-01",
         "updated_at": "2023-01-02"
     }
-    assert repo.to_dict() == expected_dict
+    assert test_repository.to_dict() == expected_dict
 
 
 def test_eq():
@@ -41,7 +41,6 @@ def test_eq():
     assert (repo == repo_diff) == False
 
 
-def test_str():
-    repo = Repository('TestRepo', 100, '2023-01-01', '2023-01-02')
+def test_str(test_repository):
     expected_str = "Repository{repositoryName=TestRepo, starsCount=100, created_at=2023-01-01, updated_at=2023-01-02}"
-    assert str(repo) == expected_str
+    assert str(test_repository) == expected_str
